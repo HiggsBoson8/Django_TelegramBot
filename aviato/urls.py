0b2493c1ae6db5ee0963dc12ff1ae12adbfbc3c9
HiggsBoson8 <chaosunundivided40k@gmail.com>
2023-03-07 16:36:34 +0600

    Django Bot Admin Update

---
+++ aviato/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path 
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render 
+from django.core.serializers.json import DjangoJSONEncoder
+
+from .forms import Geo
+from .models import Applications
+
+from loguru import logger 
+
+
+class MyJsonResponse(JsonResponse):
+    def __init__(self, data, encoder = DjangoJSONEncoder, safe = True, **kwargs):
+        json_dumps_params = dict(ensure_ascii = False)
+        super().__init__(data, encoder, safe, json_dumps_params, **kwargs)
+
+
+def index(request):
+    if request.method == 'POST':
+        id_or_phone = request.POST.get('id_or_phone') 
+
+        try:
+            p = Applications.objects.filter(pk = id_or_phone)
+            logger.success(p)
+
+            if len(p) >= 1:
+                return render(request, 'index.html',{
+                    'form': Geo()
+                    'product': p
+                })
+        except:
+            pass
+        
